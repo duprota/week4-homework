fa9dfad52f3b567fdd2384e4b569e404b4bcea7a
duprota <edelguerraprota2014@kellogg.northwestern.edu>
2014-02-05 11:15:08 -0600

    Week 4 homework - Eduardo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Week4hw::Application.routes.draw do
 
   get "/" => "pages#home"
 
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
+
+
   # HINT: Add more routes here...
 
 
